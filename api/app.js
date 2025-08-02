c291ad2d9a9d20b99b1c0c33c4350aa8ed6d311c
HAFIS-DAVIES <hafis.davies01@gmail.com>
2025-08-01 19:30:24 -0600

    healthcheck

--- api/app.js
+++ api/app.js
@@ -16,6 +16,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+
+// ✅ Health Check
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
